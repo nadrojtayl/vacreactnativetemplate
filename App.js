6fa519d6d5a16087d44f31b24346767e0b1f1daf
Jordan Taylor <nadrojtayl@hotmail.com>
2020-03-24 12:25:41 -0400

    more

--- App.js
+++ App.js
@@ -7,17 +7,17 @@ export default function App() {
 
       <View style = {{backgroundColor:"red", height:"34%", width: 
       "100%", alignItems:"center", justifyContent:'center' }}>
-         <Text style = {{color:"white" }}>Open up App.js to start working on your app!</Text>
+         <Text style = {{color:"black" }}>Open up App.js to start working on your app!</Text>
       </View>
 
        <View style = {{backgroundColor:"yellow", height:"34%", width: 
       "100%", alignItems:"center", justifyContent:'center' }}>
-         <Text style = {{color:"white" }}>Open up App.js to start working on your app!</Text>
+         <Text style = {{color:"black" }}>Open up App.js to start working on your app!</Text>
       </View>
 
        <View style = {{backgroundColor:"blue", height:"34%", width: 
       "100%", alignItems:"center", justifyContent:'center' }}>
-         <Text style = {{color:"white" }}>Open up App.js to start working on your app!</Text>
+         <Text style = {{color:"black" }}>Open up App.js to start working on your app!</Text>
       </View>
      
     </View>
